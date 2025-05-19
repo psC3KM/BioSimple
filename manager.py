151fd29329a233d7c17075fdf847082ba9f372ca
Nikolay Loskutov <Nikollosk@yandex.ru>
2025-05-19 19:54:56 +0300

    working class SampleManager1

--- manager.py
+++ manager.py
@@ -23,3 +23,34 @@ class SampleManager:
             self.samples.pop(sample_id)
             return True
         return False
+
+    def search_sample(self, **filters):
+        """
+        Поиск образцов по критериям
+        filters: Поля и их значения для фильтрации.
+        return: Список подходящих образцов.
+        """
+        result = []
+        for sample in self.samples.values():
+            match = True
+            for field, value in filters.items():
+                if getattr(sample, field) != value:
+                    match = False
+                    break
+            if match:
+                result.append(sample)
+        return result
+
+    def edit_sample(self, sample_id, **changes):
+        """
+        Редактирует поля образца по ID.
+        sample_id: ID образца.
+        changes: Поля и их новые значения.
+        return: True, если успешно, инчае False.
+        """
+        if sample_id in self.samples:
+            sample = self.samples[sample_id]
+            for field, value in changes.items():
+                setattr(sample, field, value)
+            return True
+        return False
